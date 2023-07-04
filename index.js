d043fdfaf50812f610013e37531ec21094b266cf
Elie Harfouche <elie7795@gmail.com>
2023-07-04 20:57:23 +0300

    add the old codes

--- index.js
+++ index.js
@@ -1,4 +1,28 @@
-//
+// const jsonfile = require("jsonfile");
+// const FILE_PATH = "./data.json";
+// const moment = require("moment");
+// const simpleGit = require("simple-git");
+// const random = require("random");
+// const makeCommit = (n) => {
+//   if (n === 0) return simpleGit().push();
+//   const x = random.int(0, 54);
+//   const y = random.int(0, 6);
+
+//   const DATE = moment()
+//     .subtract(1, "y")
+//     .add(1, "d")
+//     .add(x, "w")
+//     .add(y, "d")
+//     .format();
+//   const data = { date: DATE };
+//   jsonfile.writeFile(FILE_PATH, data, () => {
+//     simpleGit()
+//       .add([FILE_PATH])
+//       .commit(DATE, { "--date": DATE }, makeCommit.bind(this, --n));
+//   });
+// };
+
+// makeCommit(500);
 
 const jsonfile = require("jsonfile");
 const FILE_PATH = "./data.json";
@@ -17,7 +41,6 @@ const makeCommit = (n) => {
     .add(y, "d")
     .format();
   const data = { date: DATE };
-  console.log(DATE);
   jsonfile.writeFile(FILE_PATH, data, () => {
     simpleGit()
       .add([FILE_PATH])
